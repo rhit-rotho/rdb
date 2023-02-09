4d7aada7740507e93dda548e7d500a2dca741ab2
Omar Roth <rotho@rose-hulman.edu>
2023-02-09 12:39:41 -0500

    Full instruction flow decoding

--- src/wrapper.c
+++ src/wrapper.c
@@ -14,7 +14,6 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/prctl.h>
 #include <sys/ptrace.h>
@@ -26,11 +25,30 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "fasthash.h"
 #include "gdbstub.h"
 #include "pbvt.h"
 
+// #define CAPSTONE_DEBUG
+
+#define APP_NAME "ptc"
+
 #define STACK_SIZE (8 * 1024 * 1024)
 #define PROCMAPS_LINE_MAX_LENGTH (PATH_MAX + 100)
+
+#define HCOUNTER_ROWS (0x4)
+#define HCOUNTER_COLS (0x1000)
+#define COUNTER_SIZE (0x1000)
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+// Saturate X at +/-DERIVE_MAX
+#define DERIVE_MAX (0xfefefefe)
+#define SAT_INC(x)                                                             \
+  do {                                                                         \
+    if ((x) + 1 < DERIVE_MAX)                                                  \
+      x++;                                                                     \
+  } while (0)
 
 void *gdbstub_stk;
 pid_t ppid;
@@ -44,15 +62,141 @@ void sighandler(int signo) {
   exit(0);
 }
 
-int handle_events(struct pt_insn_decoder *decoder, int status) {
+csh handle;
+cs_insn tinsn;
+size_t insn_counter;
+int process_block(struct pt_block *block,
+                  struct pt_image_section_cache *iscache, uint64_t *fip) {
+  uint16_t ninsn;
+  uint64_t ip;
+
+  ip = block->ip;
+  for (ninsn = 0; ninsn < block->ninsn; ++ninsn) {
+    struct pt_insn insn;
+
+    memset(&insn, 0, sizeof(insn));
+    insn.speculative = block->speculative;
+    insn.isid = block->isid;
+    insn.mode = block->mode;
+    insn.ip = ip;
+
+    if (block->truncated && ((ninsn + 1) == block->ninsn)) {
+      insn.truncated = 1;
+      insn.size = block->size;
+
+      memcpy(insn.raw, block->raw, insn.size);
+    } else {
+      int size;
+      size = pt_iscache_read(iscache, insn.raw, sizeof(insn.raw), insn.isid,
+                             insn.ip);
+      if (size < 0)
+        return size;
+
+      insn.size = (uint8_t)size;
+    }
+
+#ifdef CAPSTONE_DEBUG
+    const uint8_t *code = (uint8_t *)ip;
+    uint64_t address = ip;
+    size_t sz = 0x10; // max size of x86 insn is 15 bytes
+    if (!cs_disasm_iter(handle, &code, &sz, &address, tinsn)) {
+      GDB_PRINTF("cs_disasm_iter: %s\n", cs_strerror(cs_errno(handle)));
+      break;
+    }
+    GDB_PRINTF("0x%.16lx:\t%s\t%s\n", insn.ip, tinsn->mnemonic, tinsn->op_str);
+    ip += tinsn->size;
+#else
+    ip += insn.size;
+#endif
+    insn_counter++;
+  }
+
+  *fip = ip;
+
+  return 0;
+}
+
+int process_full_trace(uint8_t *buf, size_t n,
+                       struct pt_image_section_cache *pim, int asid) {
+  // UNUSED(bitmap);
+
+  struct pt_block_decoder *decoder;
+  struct pt_config config;
+
+  memset(&config, 0, sizeof(config));
+  config.size = sizeof(config);
+  config.begin = buf;
+  config.end = buf + n;
+  config.decode.callback = NULL;
+  config.decode.context = NULL;
+
+  decoder = pt_blk_alloc_decoder(&config);
+  int pim_status =
+      pt_image_add_cached(pt_blk_get_image(decoder), pim, asid, NULL);
+  if (pim_status < 0) {
+    GDB_PRINTF("pt_image_add_cached(%d): %s\n", asid, pt_errstr(pim_status));
+    exit(-1);
+  }
+
+  uint64_t fip = 0xbad0bad0bad0bad0;
+  int wstatus = pt_blk_sync_forward(decoder);
+  if (wstatus < 0) {
+    GDB_PRINTF("%s\n", pt_errstr(-wstatus));
+    return -1;
+  }
+
+  for (;;) {
+    for (;;) {
+      struct pt_block block;
+      int errcode;
+
+      wstatus = handle_events(decoder, wstatus);
+      if (wstatus < 0) {
+        // fip = __LINE__;
+        break;
+      }
+
+      wstatus = pt_blk_next(decoder, &block, sizeof(block));
+      errcode = process_block(&block, pim, &fip);
+      if (wstatus == -pte_eos) {
+        // fip = __LINE__;
+        break;
+      }
+      if (errcode < 0)
+        wstatus = errcode;
+      if (wstatus < 0)
+        break;
+    }
+    if (wstatus == -pte_eos)
+      break;
+    if (wstatus < 0) {
+      GDB_PRINTF("%s\n", wstatus, pt_errstr(wstatus));
+      break;
+    }
+
+    wstatus = pt_blk_sync_forward(decoder);
+    if (wstatus < 0) {
+      GDB_PRINTF("%s\n", pt_errstr(-wstatus));
+      break;
+    }
+  }
+
+  GDB_PRINTF("Final ip from decode: 0x%.16lx\n", fip);
+  GDB_PRINTF("Finished processing %d instructions.\n", insn_counter);
+
+  pt_blk_free_decoder(decoder);
+
+  return 0;
+}
+
+int handle_events(struct pt_block_decoder *decoder, int status) {
   while (status & pts_event_pending) {
     struct pt_event event;
 
-    status = pt_insn_event(decoder, &event, sizeof(event));
+    status = pt_blk_event(decoder, &event, sizeof(event));
     if (status < 0)
       break;
-
-    GDB_PRINTF("event: %d\n", event.type);
+    // GDB_PRINTF("event: %d\n", event.type);
   }
 
   return status;
@@ -102,7 +246,8 @@ int gdbstub(void *args) {
   if (maps < 0)
     xperror("fopen");
 
-  struct pt_image *pim = pt_image_alloc("test-app");
+  struct pt_image_section_cache *pim = pt_iscache_alloc(NULL);
+  int asid;
 
   char buf[PROCMAPS_LINE_MAX_LENGTH];
   uint64_t image_start;
@@ -114,21 +259,27 @@ int gdbstub(void *args) {
     sscanf(buf, "%lx-%lx %4c %x %x:%x %ld %[^\n]", &from, &to, flags, &offset,
            &major, &minor, &inode, name);
 
-    // bool is_r = flags[0] == 'r';
+    bool is_r = flags[0] == 'r';
     bool is_w = flags[1] == 'w';
     bool is_x = flags[2] == 'x';
 
-    if (strstr(name, "test-app") && image_start == 0) {
+    int prot = PROT_NONE;
+    prot |= is_r ? PROT_READ : PROT_NONE;
+    prot |= is_w ? PROT_WRITE : PROT_NONE;
+    prot |= is_x ? PROT_EXEC : PROT_NONE;
+
+    if (strstr(name, APP_NAME) && image_start == 0) {
       image_start = from;
       GDB_PRINTF("Image base: %.16lx\n", image_start);
     }
 
     // PT decoder add range
-    if (is_x && strstr(name, "test-app")) {
-      GDB_PRINTF("[test-app] (offset: %.6lx) %s %lx-%lx '%s'\n",
-                 from - image_start, flags, from, to, name);
-      if (pt_image_add_file(pim, name, from - image_start, to - from, NULL,
-                            from) < 0)
+    if (is_x && strstr(name, APP_NAME)) {
+      GDB_PRINTF("(offset: %.6lx) %s %lx-%lx '%s'\n", from - image_start, flags,
+                 from, to, name);
+      asid =
+          pt_iscache_add_file(pim, name, from - image_start, to - from, from);
+      if (asid < 0)
         GDB_PRINTF("pt_image_add_file failed!\n", 0);
     }
 
@@ -142,22 +293,21 @@ int gdbstub(void *args) {
 
     if (strcmp(name, "[heap]") == 0) {
       GDB_PRINTF("[heap] %s %lx-%lx\n", flags, from, to);
-      pbvt_track_range((void *)from, to - from);
+      pbvt_track_range((void *)from, to - from, prot);
       continue;
     }
 
     if (strcmp(name, "[stack]") == 0) {
       GDB_PRINTF("[stack] %s %lx-%lx\n", flags, from, to);
-      pbvt_track_range((void *)from, to - from);
+      pbvt_track_range((void *)from, to - from, prot);
       continue;
     }
 
-    // replace userfaultfd with PROT_READ|sigsegv
-    // if (strstr(name, "test-app") != NULL) {
-    //   GDB_PRINTF("[test-app] %s %lx-%lx %s\n", flags, from, to, name);
-    //   pbvt_track_range((void *)from, to - from);
-    //   continue;
-    // }
+    if (strstr(name, APP_NAME) != NULL) {
+      GDB_PRINTF("[%s] %s %lx-%lx %s\n", APP_NAME, flags, from, to, name);
+      pbvt_track_range((void *)from, to - from, prot);
+      continue;
+    }
 
     GDB_PRINTF("[ignore] %s %lx-%lx %s\n", flags, from, to, name);
   }
@@ -175,6 +325,9 @@ int gdbstub(void *args) {
   ctx->stopped = 1;
   ctx->regs = pbvt_calloc(1, sizeof(struct user_regs_struct));
   ctx->fpregs = pbvt_calloc(1, sizeof(struct user_fpregs_struct));
+
+  // TODO: Calculate this based on size of executable memory
+  ctx->bitmap = pbvt_calloc(0x1000, sizeof(uint8_t));
   gdb_save_state(ctx);
 
   xptrace(PTRACE_CONT, ctx->ppid, NULL, NULL);
@@ -202,9 +355,8 @@ int gdbstub(void *args) {
   // /sys/bus/event_source/devices/intel_pt/format/branch
   attr.config |= 1 << 13;
   // /sys/bus/event_source/devices/intel_pt/format/psb_period
-  attr.config |= 0 << 24;
-
   // PSB period: expect every 2**(value+11) bytes
+  attr.config |= 0 << 24;
 
   attr.disabled = 0;
 
@@ -214,7 +366,8 @@ int gdbstub(void *args) {
 
   struct perf_event_mmap_page *header;
   void *base, *data, *aux;
-  int n = 0, m = 16; // data size, aux size
+  UNUSED(data);
+  int n = 0, m = 32; // data size, aux size
 
   base = mmap(NULL, (1 + 2 * n) * PAGE_SIZE, PROT_WRITE, MAP_SHARED, pfd, 0);
   if (base == MAP_FAILED)
@@ -228,101 +381,51 @@ int gdbstub(void *args) {
 
   // PROT_READ - circular buffer
   // PROT_READ|PROT_WRITE - linear buffer
-  aux = mmap(NULL, header->aux_size, PROT_READ | PROT_WRITE, MAP_SHARED, pfd,
+  aux = mmap(NULL, header->aux_size, PROT_READ, MAP_SHARED, pfd,
              header->aux_offset);
   if (aux == MAP_FAILED)
     xperror("mmap");
 
-  GDB_PRINTF("aux_size:%lx\n", header->aux_size);
-  GDB_PRINTF("aux_head:%lx\n", header->aux_head);
+  int xstatus;
+  insn_counter = 0;
 
-  // xptrace(PTRACE_SYSCALL, ctx->ppid, NULL, NULL);
-  xptrace(PTRACE_CONT, ctx->ppid, NULL, NULL);
-  // xptrace(PTRACE_SINGLESTEP, ctx->ppid, NULL, NULL);
-  usleep(100);
-  xptrace(PTRACE_INTERRUPT, ctx->ppid, NULL, NULL);
-  waitpid(ctx->ppid, &status, 0);
-
-  struct pt_insn_decoder *decoder;
-  struct pt_config config;
-  int errcode;
-
-  memset(&config, 0, sizeof(config));
-  config.size = sizeof(config);
-  config.begin = aux;
-  config.end = aux + header->aux_size;
-  config.decode.callback = NULL;
-  config.decode.context = NULL;
-
-  decoder = pt_insn_alloc_decoder(&config);
-  GDB_PRINTF("decoder: %p\n", decoder);
-  GDB_PRINTF("pt_insn_set_image(decoder, pim) => %d\n",
-             pt_insn_set_image(decoder, pim));
-
-  csh handle;
-  if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK)
-    xperror("cs_open");
+#ifdef CAPSTONE_DEBUG
+  if (cs_open(CS_ARCH_X86, CS_MODE_64, &handle) != CS_ERR_OK) {
+    GDB_PRINTF("cs_open: %s\n", cs_strerror(cs_errno(handle)));
+    return -1;
+  }
   cs_option(handle, CS_OPT_DETAIL, CS_OPT_ON);
-  cs_insn *tinsn = cs_malloc(handle);
+  tinsn = cs_malloc(handle);
+#endif
+  uint8_t ptbuf[0x10000];
 
-  for (;;) {
-    // printf("%.16lx\n", header->aux_head);
-    if (header->aux_head != header->aux_tail) {
-      GDB_PRINTF("head: %p\n", header->aux_head);
-      GDB_PRINTF("tail: %p\n", header->aux_tail);
-      for (int j = 0; j < 5; ++j) {
-        GDB_PRINTF("", 0);
-        for (int i = 0; i < 0x20; ++i)
-          printf("%.2x ", ((uint8_t *)aux)[j * 0x20 + i]);
-        printf("\n");
-      }
-      int insn_status = pt_insn_sync_forward(decoder);
+  xptrace(PTRACE_SYSCALL, ctx->ppid, NULL, NULL);
+  waitpid(ctx->ppid, &xstatus, 0);
+  if (ioctl(pfd, PERF_EVENT_IOC_DISABLE, 0) < 0)
+    xperror("ioctl(PERF_EVENT_IOC_DISABLE)");
 
-      for (;;) {
-        struct pt_insn insn;
+  memcpy(ptbuf, aux + header->aux_tail, header->aux_head - header->aux_tail);
+  for (int i = 0; i < header->aux_head - header->aux_tail; ++i) {
+    printf("%.2x ", ((uint8_t *)ptbuf)[i]);
+  }
+  printf("\n");
 
-        insn_status = handle_events(decoder, insn_status);
-        if (insn_status < 0) {
-          GDB_PRINTF("%d %s\n", insn_status, pt_errstr(-insn_status));
-          break;
-        }
+  GDB_PRINTF("%.16lx %.16lx\n", header->aux_tail, header->aux_head);
 
-        errcode = pt_insn_next(decoder, &insn, sizeof(insn));
-        if (errcode < 0) {
-          GDB_PRINTF("%d %s\n", errcode, pt_errstr(-errcode));
-          if (-errcode == pte_eos)
-            goto pt_end;
+  struct user_regs_struct xregs = {0};
+  xptrace(PTRACE_GETREGS, ctx->ppid, NULL, &xregs);
+  // Back up one, since we hit a trap
+  GDB_PRINTF("Process ended at rip: 0x%0.16lx\n", xregs.rip - 1);
 
-          break;
-        }
+  process_full_trace(ptbuf, header->aux_head - header->aux_tail, pim, asid);
+  header->aux_tail = header->aux_head;
 
-        uint64_t *code = insn.ip;
-        uint64_t address = insn.ip;
-        size_t sz = 0x10; // max size of x86 insn is 15 bytes
-        if (!cs_disasm_iter(handle, &code, &sz, &address, tinsn)) {
-          GDB_PRINTF("fail: %s\n", cs_strerror(cs_errno(handle)));
-          break;
-        }
-        // GDB_PRINTF("0x%.16lx:\t%s\t%s\n", insn.ip, tinsn->mnemonic,
-        // tinsn->op_str);
-      }
-      uint64_t insn_offset;
-      pt_insn_get_offset(decoder, &insn_offset);
-      GDB_PRINTF("insn_offset: %p, aux_size: %p\n", insn_offset,
-                 header->aux_size);
-      // memmove(aux, aux+insn_offset, header->aux_size - insn_offset);
-      header->aux_tail = insn_offset;
-      // header->aux_head = header->aux_size - insn_offset;
-      // pt_insn_sync_set(decoder, 0);
-      // break;
-    }
+  for (int i = 0; i < 3; ++i) {
+    xptrace(PTRACE_SYSCALL, ctx->ppid, NULL, NULL);
+    waitpid(ctx->ppid, &xstatus, 0);
   }
 
-pt_end:
-  GDB_PRINTF("Done.\n", 0);
   return 0;
-
-  // ioctl(pfd, PERF_EVENT_IOC_DISABLE, 0);
 
   uint64_t rd_val;
   int nbytes = read(pfd, &rd_val, sizeof(rd_val));
@@ -361,12 +464,12 @@ pt_end:
 
   int timerfd = timerfd_create(CLOCK_REALTIME, 0);
   struct itimerspec arm = {0};
-  arm.it_interval.tv_sec = 10;
-  arm.it_interval.tv_nsec = 0;
-  // arm.it_interval.tv_nsec = 1000 * 1000 * 10;
-  arm.it_value.tv_sec = 10;
+  // arm.it_interval.tv_sec = 10;
+  // arm.it_interval.tv_nsec = 0;
+  arm.it_interval.tv_sec = 0;
+  arm.it_interval.tv_nsec = 1000 * 1000 * 10;
   arm.it_value.tv_nsec = 0;
-  // arm.it_value.tv_nsec = 1000 * 1000 * 10;
+  arm.it_value.tv_nsec = 1000 * 1000 * 10;
   if (timerfd_settime(timerfd, 0, &arm, NULL) < 0)
     xperror("timerfd_settime");
 
@@ -411,8 +514,8 @@ pt_end:
       continue;
     }
 
-    // Process hit syscall, handle this before our timer in case the process is
-    // already stopped on a syscall.
+    // Process hit syscall, handle this before our timer in case the process
+    // is already stopped on a syscall.
     if (pollfds[1].revents & POLLERR)
       xperror("POLLERR in signalfd");
     if (pollfds[1].revents & POLLIN) {
@@ -453,6 +556,7 @@ pt_end:
     if (pollfds[2].revents & POLLERR)
       xperror("POLLERR in timer");
     if (pollfds[2].revents & POLLIN) {
+      printf("Handle trace!\n");
       uint64_t expiry;
       read(pollfds[2].fd, &expiry, sizeof(expiry));
 

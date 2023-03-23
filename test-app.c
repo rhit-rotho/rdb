9616a3154c960dbb08ba431b1cef6db5430b10e6
Omar Roth <rotho@rose-hulman.edu>
2023-03-23 00:49:24 -0400

    Update test-app

--- test-app.c
+++ test-app.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #define ARR_SIZE (0x100)
@@ -28,9 +29,6 @@ void sighandler(int signo) {
 }
 
 // TODO: seed with incorrect values
-uint8_t trand[0x100];
-
-#include <time.h>
 double get_time() {
   struct timespec tp;
   clock_gettime(CLOCK_REALTIME, &tp);
@@ -41,17 +39,17 @@ double get_time() {
 int main(int argc, char **argv) {
   // signal(SIGINT, sighandler);
 
-  // srand(time(0));
-  srand(0);
-  for (int i = 0; i < sizeof(trand) / sizeof(trand[0]); ++i)
-    trand[i] = rand();
+  // srand(0);
+  // for (int i = 0; i < sizeof(trand) / sizeof(trand[0]); ++i)
+  //   trand[i] = rand();
 
-  if (argc < 2)
-    return -1;
+  // if (argc < 2)
+  //   return -1;
 
-  double start = get_time();
-  if (argv[1][0] == 'a')
-    asm("int3");
+  // double start = get_time();
+  double start = 0;
+  // if (argv[1][0] == 'a')
+  asm("int3");
 
   char str[] = "hello";
   char S[ARR_SIZE] = {0};
@@ -59,16 +57,43 @@ int main(int argc, char **argv) {
   UNUSED(S);
 
   int a = 0;
-  // for (uint64_t counter = 0; counter < 0x10000000; ++counter) {
-  for (uint64_t counter = 0; counter < 0x1000; ++counter) {
-    if (a % 2 == 0)
-      a = trand[a];
-    else
-      a = trand[a] ^ trand[(a + 1) % 0x100];
+  for (uint64_t counter = 0;; ++counter) {
+    a += 255;
+    a += 15;
+    a += 248;
+    a += 233;
+    a += 142;
+    a += 27;
+    a += 249;
+    a += 127;
+    a += 92;
+    a += 64;
+    a += 70;
+    a += 15;
+    a += 252;
+    a += 40;
+    a += 19;
+    a += 97;
+    a += 59;
+    a += 9;
+    a += 2;
+    a += 217;
+    a += 239;
+    a += 29;
+    a += 248;
+    a += 69;
+    a += 88;
+    a += 141;
+    a += 49;
+    a += 107;
+    a += 220;
+    a += 161;
+    a += 100;
+    a += 8;
   }
 
-  if (argv[1][0] == 'a')
-    asm("int3");
+  // if (argv[1][0] == 'a')
+  //   asm("int3");
 
   double end = get_time();
   TEST_PRINTF("Elapsed: %lf seconds\n", end - start);

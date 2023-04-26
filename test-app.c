faba250b66bb562b96d6b648d6035e62d3ccec97
Omar Roth <rotho@rose-hulman.edu>
2023-04-26 15:03:36 -0400

    Update test appliace

--- test-app.c
+++ test-app.c
@@ -21,82 +21,106 @@
       fprintf(stderr, TEST_PRINTF_HEADER fmt, getpid(), __VA_ARGS__);          \
   } while (0)
 
-#include <time.h>
 void sighandler(int signo) {
   UNUSED(signo);
   TEST_PRINTF("I got the signal!\n", 0);
   exit(0);
 }
 
-// TODO: seed with incorrect values
+int call1() { return 5; }
+
+int call2() {
+  int result = 0;
+  result += call1();
+  result += call1();
+  return result;
+}
+
 double get_time() {
-  struct timespec tp;
-  clock_gettime(CLOCK_REALTIME, &tp);
+  struct timespec tp = {0};
+  // clock_gettime(CLOCK_REALTIME, &tp);
+  tp.tv_sec = (double)call2();
   return tp.tv_sec + tp.tv_nsec * 1e-9;
 }
 
+// TODO: seed with incorrect values
 // generate some memory traffic
 int main(int argc, char **argv) {
-  // signal(SIGINT, sighandler);
 
-  // srand(0);
-  // for (int i = 0; i < sizeof(trand) / sizeof(trand[0]); ++i)
-  //   trand[i] = rand();
-
-  // if (argc < 2)
-  //   return -1;
-
-  // double start = get_time();
-  double start = 0;
-  // if (argv[1][0] == 'a')
-  asm("int3");
+  double start = get_time();
+  if (argc < 2)
+    asm("int3");
 
   char str[] = "hello";
   char S[ARR_SIZE] = {0};
   UNUSED(str);
   UNUSED(S);
 
-  int a = 0;
-  for (uint64_t counter = 0;; ++counter) {
-    a += 255;
-    a += 15;
-    a += 248;
-    a += 233;
-    a += 142;
-    a += 27;
-    a += 249;
-    a += 127;
-    a += 92;
-    a += 64;
-    a += 70;
-    a += 15;
-    a += 252;
-    a += 40;
-    a += 19;
-    a += 97;
-    a += 59;
-    a += 9;
-    a += 2;
-    a += 217;
-    a += 239;
-    a += 29;
-    a += 248;
-    a += 69;
-    a += 88;
-    a += 141;
-    a += 49;
-    a += 107;
-    a += 220;
-    a += 161;
-    a += 100;
-    a += 8;
+  int other = 0;
+  for (uint64_t counter_b = 0; counter_b < 0x10000; ++counter_b) {
+    for (uint64_t counter_c = 0; counter_c < 0x800; ++counter_c) {
+      other *= 11;
+      other += 17;
+    }
   }
 
-  // if (argv[1][0] == 'a')
-  //   asm("int3");
+  int val = 0;
+  for (uint64_t counter = 0; counter < 0x500000; ++counter) {
+    int other = 0;
+    for (uint64_t counter_b = 0; counter_b < 0x10; ++counter_b) {
+      for (uint64_t counter_c = 0; counter_c < 0x8; ++counter_c) {
+        other *= 11;
+        other += 17;
+      }
+    }
 
+    for (uint64_t counter_b = 0; counter_b < 0x20; ++counter_b) {
+      for (uint64_t counter_c = 0; counter_c < 0x10; ++counter_c) {
+        other *= 11;
+        other += 17;
+      }
+    }
+
+    val += 255;
+    val += 15;
+    val += 248;
+    val += 233;
+    val += 142;
+    val += 27;
+    val += 249;
+    val += 127;
+    val += 92;
+    val += 64;
+    val += 70;
+    val += 15;
+    val += 252;
+    val += 40;
+    val += 19;
+    val += 97;
+    val += 59;
+    val += 9;
+    val += 2;
+    val += 217;
+    val += 239;
+    val += 29;
+    val += 248;
+    val += 69;
+    val += 88;
+    val += 141;
+    val += 49;
+    val += 107;
+    val += 220;
+    val += 161;
+    val += 100;
+    val += 8;
+  }
+
+  if (argc < 2)
+    asm("int3");
   double end = get_time();
-  TEST_PRINTF("Elapsed: %lf seconds\n", end - start);
+  double elapsed = end - start;
+
+  TEST_PRINTF("Elapsed: %lf seconds\n", elapsed);
   TEST_PRINTF("Success!\n", 0);
   return 0;
 }
